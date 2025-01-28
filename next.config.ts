a051e4dc58b374b58d5454f88cd72d58a2eda566
Perry Fardella <perry.fardella@live.com>
2025-01-28 13:49:19 +0700

    Move blogs, get mdx set up and working
    HTML elements are rendering correctly, Im just not currently styling my html elements at all. Need to add Typography.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import nextMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ["mdx", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = nextMDX({
+  // Add markdown plugins here, as desired
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
