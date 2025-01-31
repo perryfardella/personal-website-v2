5873c46b1645b82bf731f8462dd68f9e25ddd26a
Perry Fardella <perry.fardella@live.com>
2025-01-31 20:21:22 +0700

    Remove unused pathname code from nav bar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,30 +1,13 @@
 "use client";
 
 import Link from "next/link";
-// import { usePathname } from "next/navigation";
 import { ThemeToggle } from "./ThemeToggle";
 
 export function NavBar() {
-  // const pathname = usePathname();
-
-  const navLinks = [
-    { href: "/", label: "Perry Fardella" },
-    // { href: "/blog", label: "Blog" },
-    // { href: "/projects", label: "Projects" },
-  ];
-
-  // const isActive = (href: string) => {
-  //   if (href === "/") {
-  //     return pathname === href;
-  //   }
-  //   return pathname.startsWith(href);
-  // };
-
+  const navLinks = [{ href: "/", label: "Perry Fardella" }];
   return (
     <nav className="flex w-full mb-6 justify-between">
-      {/* <ul className="flex gap-4"> */}
       {navLinks.map((link) => (
-        // <li key={link.href} className={`hover:opacity-60 transition-opacity`}>
         <Link
           href={link.href}
           key={link.href}
@@ -32,9 +15,7 @@ export function NavBar() {
         >
           <h1>{link.label}</h1>
         </Link>
-        // </li>
       ))}
-      {/* </ul> */}
       <ThemeToggle />
     </nav>
   );
